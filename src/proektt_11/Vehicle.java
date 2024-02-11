e027a54e9a65760f37728de7e51e8fe966634e9d
EvaIlievska <ilievska_eva@yahoo.com>
2024-02-11 09:27:35 +0100

    vezba24

---
+++ src/proektt_11/Vehicle.java
@@ -0,0 +1,57 @@
+package proektt_11;
+
+public class Vehicle {
+	private String color;
+	private int x;
+	private String b;
+	private int y;
+	
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public String getColor1() {
+		return color;
+	}
+
+	public void setColor1(String color) {
+		this.color = color;
+	}
+
+	public void setColor(String color2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setColor11(String color2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
